8cc1eb0e8d4987c8b7ddb21b70a707030d0444fa
mdasif8393 <md.asif.8393@gmail.com>
2025-09-06 23:13:14 +0600

    add admin side bars insde sidebars

---
+++ src/routes/adminSidebarItems.ts
@@ -0,0 +1,20 @@
+export const adminSidebarItems = [
+  {
+    title: "Manage Users",
+    items: [
+      {
+        title: "All Users",
+        url: "/admin/all-users",
+      },
+    ],
+  },
+  {
+    title: "Manage Agents",
+    items: [
+      {
+        title: "Agent Status",
+        url: "/admin/agent-status",
+      },
+    ],
+  },
+];
